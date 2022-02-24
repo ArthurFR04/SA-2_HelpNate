c9944b50af8d88e950e0b21b83c250371aa29706
ismael-back <ismaelsktcore9898@hotmail.com>
2022-02-23 17:52:15 -0700

    Solving reanimated error

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native";
 import RegistrationScreen from './src/screens/Registration/RegistrationScreen';
 
 export default function App() {
