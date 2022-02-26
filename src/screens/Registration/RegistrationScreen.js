df3720ffef1ab6052b23a782a0a3bab2b274e055
ismael-back <ismaelsktcore9898@hotmail.com>
2022-02-25 17:57:58 -0700

    Update Style Registration Screen

--- src/screens/Registration/RegistrationScreen.js
+++ src/screens/Registration/RegistrationScreen.js
@@ -2,19 +2,21 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import style from './RegistrationStyle'; 
 import { Link } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export const RegistrationScreen = () =>{
     return(
         <View style={style.container}>
-            <View style={style.containerCircle}>
-                <Text>oi</Text>
-
-                <Link to={{ screen: 'Login'}}>
-                    Dale pro loginzinho
-                </Link>
-
-                <View style={style.circle}><Text>oi</Text></View>
-                
+            <View style={style.circle}>
+                <View style={style.containerCircle}></View>
+                <View style={style.logoCircle}>
+                    <Link to={{ screen: 'Login'}}>
+                        LOGIN
+                    </Link>
+                </View>
+                <View style={style.arrow}>
+                    <FontAwesome name="long-arrow-down" size={36} color="#F2F2F2" />
+                </View>   
             </View>
         </View>
     )   
