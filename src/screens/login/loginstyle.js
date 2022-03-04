de44516d7fa8734e96d6564ae1aff9242877542a
dj-orp <88634048+dj-orp@users.noreply.github.com>
2022-03-04 12:26:25 -0600

    updates

--- src/screens/login/loginstyle.js
+++ src/screens/login/loginstyle.js
@@ -1,51 +1,66 @@
-import { StyleSheet } from 'react-native';
-import { color, not } from 'react-native-reanimated';
+import { StyleSheet, Dimensions } from 'react-native';
 
 export default StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: '#FFEF6D'
     },
     conteudo: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
     },
+    text: {
+        fontSize: 65,
+        marginBottom: '20%',
+        color: '#3c3c3c',
+    },
     input: {
         backgroundColor: '#f2bc1b',
-        width: 250,
+        width: 280,
         height: 55,
         padding: '2%',
         marginBottom: 45,
         color: '#3c3c3c',
-        fontSize: 17,
-        borderRadius: 10
-    },
-    text: {
-        fontSize: 40,
-        marginBottom: '30%',
-        color: '#3c3c3c',
+        fontSize: 20,
+        borderRadius: 10,
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 3.62,
+        elevation: 4,
     },
     btnLogar: {
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-        width: 150,
+        width: 160,
         height: 45,
         borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 8,
     },
     textbtn: {
-        fontSize: 22,
+        fontSize: 24,
         color: '#3c3c3c',
     },
 
     conteudo2: {
         backgroundColor: 'white',
         width: 500,
-        height: '35%',
-        borderRadius: 200,
+        height: '32%',
+        //borderRadius: 200,
+        borderTopEndRadius: Dimensions.get('window').width / 2,
+        borderTopStartRadius: Dimensions.get('window').width / 2,
         transform: [
             // Transform the origin down
             { translateY: 200 / 2 },
@@ -58,7 +73,7 @@ export default StyleSheet.create({
             height: 1,
         },
         shadowOpacity: 0.23,
-        shadowRadius: 2.62,
+        shadowRadius: 5.62,
         elevation: 8,
     },
 
@@ -66,28 +81,26 @@ export default StyleSheet.create({
         justifyContent: 'center',
         alignSelf: 'center',
         backgroundColor: 'white',
-        height: 100,
-        width: '20%',
+        height: 75,
+        width: 75,
         borderRadius: 50,
         transform: [
             // Transform the origin down
             { translateY: 0 / 2 },
             // Transform the origin back up
-            { translateY: -100 / 2 },
+            { translateY: -60 / 2 },
         ],
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
-            height: 2,
+            height: 0,
         },
         shadowOpacity: 0.23,
-        shadowRadius: 2.62,
+        shadowRadius: 5.62,
         elevation: 4,
     },
 
     arrow: {
-        marginLeft: 10,
-        marginRight: 10,
         justifyContent: 'center',
         alignItems: 'center',
     },
@@ -97,7 +110,7 @@ export default StyleSheet.create({
         alignItems: 'flex-end',
         justifyContent: 'center',
         flexDirection: 'row',
-        marginTop: '3%',
+        marginTop: '7%',
     },
 
     googlee: {
@@ -105,9 +118,9 @@ export default StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#f2bc1b',
-        marginRight: '5%',
-        width: 80,
-        height: 80,
+        marginRight: '10%',
+        width: 70,
+        height: 70,
         borderRadius: 50,
         shadowColor: "#000",
         shadowOffset: {
@@ -124,8 +137,8 @@ export default StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#f2bc1b',
-        width: 80,
-        height: 80,
+        width: 70,
+        height: 70,
         borderRadius: 50,
         shadowColor: "#000",
         shadowOffset: {
