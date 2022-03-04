a5940a5d0342bbd14c50ed8935f4816b7673afc6
dj-orp <88634048+dj-orp@users.noreply.github.com>
2022-03-04 14:58:40 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import * as React from "react";
-import { SafeAreaView, useWindowDimensions  } from "react-native";
-import {RegistrationScreen} from './src/screens/Registration/RegistrationScreen';
-import LoginScreen from './src/screens/Login/LoginScreen';
+import { SafeAreaView, useWindowDimensions } from "react-native";
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from "@react-navigation/native";
+
+import { RegistrationScreen } from './src/screens/Registration/RegistrationScreen';
+import LoginScreen from './src/screens/login/LoginScreen';
+import NewAnuncioScreen from './src/screens/NewAnuncio/NewAnuncioScreen';
+import ProfileScreen from './src/screens/Profile/ProfileScreen'
 
 const Drawer = createDrawerNavigator()
 
@@ -12,20 +15,22 @@ const App = () => {
   const dimensions = useWindowDimensions();
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
         <Drawer.Navigator initialRouteName="Registration" screenOptions={{
-            headerStyle:{backgroundColor:"#f2bc1b"},
-            height: 80,
-            drawerType: dimensions.width >= 821 ? 'permanent' : 'front',
-            overlayColor: 'transparent'
-          }}>
-          <Drawer.Screen name="Registration" component={RegistrationScreen} options={{ headerShown: false }}/>
-          <Drawer.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
+          headerStyle: { backgroundColor: "#f2bc1b" },
+          height: 80,
+          drawerType: dimensions.width >= 821 ? 'permanent' : 'front',
+          overlayColor: 'transparent'
+        }}>
+          <Drawer.Screen name="Registration" component={RegistrationScreen} options={{ headerShown: false }} />
+          <Drawer.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          <Drawer.Screen name="Perfil" component={ProfileScreen} />
+          <Drawer.Screen name="Criar Anúncio" component={NewAnuncioScreen} />
         </Drawer.Navigator>
       </NavigationContainer>
     </SafeAreaView>
-  );  
+  );
 }
 
 export default App;
