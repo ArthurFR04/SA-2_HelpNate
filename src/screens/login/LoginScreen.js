024264276487130904af01a16c81d4dcd5e16b58
dj-orp <88634048+dj-orp@users.noreply.github.com>
2022-03-04 12:25:27 -0600

    update

--- src/screens/login/LoginScreen.js
+++ src/screens/login/LoginScreen.js
@@ -1,44 +1,47 @@
 import React from 'react'
-import { View, TextInput, Text, Button, TouchableOpacity} from "react-native";
-import style from './loginstyle'
+import { View, TextInput, Text, TouchableOpacity } from "react-native";
+import style from './Loginstyle'
 import { Link } from '@react-navigation/native';
 import { FontAwesome } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function LoginScreen() {
   return (
     <View className="container" style={style.container}>
-      <View className="conteudo" style={style.conteudo}>
-        <Text style={style.text} className="text">Login</Text>
-        <TextInput style={style.input} placeholder="Insira seu nome" placeholderTextColor="#3c3c3c"  autoCorrect={false}></TextInput>
-        <TextInput  style={style.input} placeholder="Insira sua senha" placeholderTextColor="#3c3c3c" autoCorrect={false}></TextInput>
-        <TouchableOpacity style={style.btnLogar}>
-          <Text style={style.textbtn}>Logar</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View className="conteudo-2" style={style.conteudo2}>
-        
-        <View style={style.arrowG}>
-          <TouchableOpacity style={style.arrow}>
-            <Link to={{screen:'Registration'}}>
-              <FontAwesome name="long-arrow-up" size={60} color="#3c3c3c"/>
-            </Link>
+      <LinearGradient colors={['#f2bc1b', '#fff395']} style={style.background} start={{ x: 0, y: 1 }} end={{ x: -1, y: 0 }}>
+        <View className="conteudo" style={style.conteudo}>
+          <Text style={style.text} className="text">Login</Text>
+          <TextInput style={style.input} placeholder="Insira seu nome" placeholderTextColor="#3c3c3c" autoCorrect={false}></TextInput>
+          <TextInput style={style.input} placeholder="Insira sua senha" placeholderTextColor="#3c3c3c" autoCorrect={false}></TextInput>
+          <TouchableOpacity style={style.btnLogar}>
+            <Text style={style.textbtn}>Logar</Text>
           </TouchableOpacity>
         </View>
-        <View className="butonss" style={style.butons}>
-          <View style={style.googlee}>
-            <TouchableOpacity style={style.butons1}>
-              <AntDesign name="google" size={60} color="#3c3c3c" />
+
+        <View className="conteudo-2" style={style.conteudo2}>
+
+          <View style={style.arrowG}>
+            <TouchableOpacity style={style.arrow}>
+              <Link to={{ screen: 'Registration' }}>
+                <FontAwesome name="long-arrow-up" size={54} color="#3c3c3c" />
+              </Link>
             </TouchableOpacity>
           </View>
-          <View style={style.facebookk}>
-            <TouchableOpacity>
-              <FontAwesome name="facebook-f" size={60} color="#3c3c3c" />
-            </TouchableOpacity>
+          <View className="butonss" style={style.butons}>
+            <View style={style.googlee}>
+              <TouchableOpacity>
+                <AntDesign name="google" size={50} color="#3c3c3c" />
+              </TouchableOpacity>
+            </View>
+            <View style={style.facebookk}>
+              <TouchableOpacity>
+                <FontAwesome name="facebook-f" size={50} color="#3c3c3c" />
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
-      </View>
+      </LinearGradient>
     </View>
   );
 }
