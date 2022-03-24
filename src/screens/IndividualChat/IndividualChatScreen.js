49ac852ada5e205f2cfeec821886e3fb8f53e74e
ArthurSenai <arthur_f_rodrigues@estudante.sesisenai.org.br>
2022-03-24 15:51:07 -0300

    telaChatPronta

--- src/screens/IndividualChat/IndividualChatScreen.js
+++ src/screens/IndividualChat/IndividualChatScreen.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import {View, Text, Image, TextInput, TouchableOpacity, ScrollView} from 'react-native';
-import style from './IndividualChatStyle'; 
+import { View, Text, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import style from './IndividualChatStyle';
 import { Link } from '@react-navigation/native';
 
-export const IndividualChatScreen = () =>{
-    return(
+export const IndividualChatScreen = () => {
+    return (
         <View style={style.container}>
-            <Text>Individual Chat Screen</Text>
+            <ScrollView>
+                <View className="chatDestinatario" style={style.destinatario}>
+                    <Text style={style.text}>Olá boa tarde, estou aqui para perguntar se aquele foquete ainda esta para doação? E gostaria de ver algumas imagens dele para ver se está tudo certo.</Text>
+                </View>
+                <View className="chatDonativo" style={style.donativo}>
+                    <Text style={style.text}>Olá já irei te passar as fotos, estou doando o foguete pois nao tenho mais utilidade para ele e sei que alguem vai ter!</Text>
+                </View>
+                <View className="chatDonativo" style={style.donativo}>
+                    <Text style={style.text}>Estou te passando as imagens agora para você ver, segue em anexo:</Text>
+                </View>
+                <View  className="chatDonativo" style={style.donativoIMG}>
+                    <Image style={style.image} source={require("../../../assets/images/fogueteSpaceX.jpg")}></Image>
+                </View>
+                <View className="chatDonativo" style={style.donativoIMG}>
+                    <Image style={style.image} source={require("../../../assets/images/fogueteSpaceX1.jpg")}></Image>
+                </View>
+                <View className="chatDestinatario" style={style.destinatario}>
+                    <Text style={style.text}>Nossa gostei muito desse foguete gostaria de marcar o local onde posso pega-lo!</Text>
+                </View>
+                <View className="chatDestinatario" style={style.destinatario}>
+                    <Text style={style.text}>Qual o tamanho dele, é de brinquedo né?</Text>
+                </View>
+                <View className="chatDonativo" style={style.donativo}>
+                    <Text style={style.text}>Tenho um foguete de verdade mas posso lhe entregar um de brinquedo! O de verdade nao teria onde você deixar então estou apenas doando o de brinquedo</Text>
+                </View>
+            </ScrollView>
         </View>
-    )   
+    )
 }
--- src/screens/IndividualChat/IndividualChatStyle.js
+++ src/screens/IndividualChat/IndividualChatStyle.js
@@ -1,7 +1,60 @@
 import { StyleSheet, Dimensions, Platform } from "react-native";
 
 export default StyleSheet.create({
-    container:{
-        flex: 1
+    container: {
+        flex: 1,
+        backgroundColor: '#e5e5e5'
     },
-})
+    destinatario: {
+        backgroundColor: '#fff',
+        maxWidth: '65%',
+        marginTop: '2%',
+        marginBottom: '2%',
+        padding: '2.2%',
+        marginLeft: '2%',
+        borderRadius: 8,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 1 },
+        shadowRadius: 4,
+        shadowOpacity: 0.2,
+        elevation: 3
+
+    },
+    donativo: {
+        backgroundColor: '#FFF395',
+        maxWidth: '65%',
+        marginTop: '2%',
+        marginBottom: '2%',
+        marginRight: '2%',
+        alignSelf: 'flex-end',
+        padding: '2.2%',
+        borderRadius: 8,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 1 },
+        shadowRadius: 4,
+        shadowOpacity: 0.2,
+        elevation: 3
+    },
+    text: {
+        fontSize: 16
+    },
+    donativoIMG:{
+        backgroundColor: '#FFF395',
+        maxWidth: '65%',
+        marginTop: '2%',
+        marginBottom: '2%',
+        marginRight: '2%',
+        alignSelf: 'flex-end',
+        padding: '2.2%',
+        borderRadius: 8,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 1 },
+        shadowRadius: 4,
+        shadowOpacity: 0.2,
+        elevation: 3
+    },
+    image:{
+        maxWidth: '100%',
+        maxHeight:150,
+    }
+});
